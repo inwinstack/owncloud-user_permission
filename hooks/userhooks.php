2cd499aee5a2b5fd7580b4b7706cea8307f3e8f7
pcp3307 <dino.p@inwinstack.com>
2016-10-14 16:14:38 +0800

    [Feature] delete all user containers after delete user

--- hooks/userhooks.php
+++ hooks/userhooks.php
@@ -9,21 +9,40 @@ class UserHooks {
     private $userManager;
    
 
-    public function __construct($userManager){
+    public function __construct($userManager) {
         $this->userManager = $userManager;
     }
 
 
-    public function EnableUser($user, $password){
+    public function EnableUser($user) {
         $uid = $user->getUID();
         $config = \OC::$server->getConfig();
         $config->setUserValue($uid, 'core', 'enabled', 'true');
     }
- 
+
+
     
-    public function register(){
+    public function register() {
         $callback = array($this, 'EnableUser');
         $this->userManager->listen('\OC\User', 'postCreateUser', $callback);
+        $DeleteAllContainer = function($user) {
+            $url = "http://140.129.25.141/dockertest/api/v1/removeUserContainers";
+            $uid = $user->getUID();
+
+            $c = curl_init();
+            $data = json_encode(array("username" => $uid));
+            curl_setopt($c, CURLOPT_POST, true);
+            curl_setopt($c, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($c, CURLOPT_URL, $url);
+            curl_setopt($c, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+            curl_setopt($c, CURLOPT_RETURNTRANSFER,1);
+
+            curl_exec($c);
+
+            $info = curl_getinfo($c);
+            curl_close($c);
+        };
+        $this->userManager->listen('\OC\User', 'postDelete', $DeleteAllContainer);
     }
 
 }
